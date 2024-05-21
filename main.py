dc3e6e7cfa2205df1e40da4c58c07c571e297ff8
off-nadir-insight <off.nadir.insight@gmail.com>
2024-05-21 11:57:25 -0700

    adds book report with formatted and sorted list

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 def main():
     book_path = "books/frankenstein.txt"
     book_text = get_book_text(book_path)
-    first_499_characters = book_text[:499]
-    print(f"{get_word_count(book_text)} words in the book.")
+    book_word_count = get_word_count(book_text)
     char_dict = get_char_count(book_text)
-    print(f"p: {char_dict["p"]}")
-    print(f"r: {char_dict["r"]}")
-    print(f"o: {char_dict["o"]}")
+    list_for_sort = create_dict_list(char_dict)
+    list_for_sort.sort(reverse=True, key=sort_on)
+    book_report(book_path, book_word_count, list_for_sort)
 
 def get_book_text(path):
     with open(path) as file:
@@ -19,11 +18,29 @@ def get_word_count(input_string):
 def get_char_count(input_string):
     char_count = {}
     for char in input_string:
+        if not char.isalpha():
+            continue
         char = char.lower()
         if char in char_count:
             char_count[char] += 1
         else:
             char_count[char] = 1
     return char_count
+
+def book_report(book_path="", word_count=0, char_list=[]):
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{word_count} words found in the document\n")
+    for char in char_list:
+        print(f"The '{char["char"]}' character was found {char["count"]} times")
+    print(f"--- End report ---")
+
+def sort_on(dict):
+    return dict["count"]
+
+def create_dict_list(dict):
+    list_of_dicts = []
+    for char in dict.keys():
+        list_of_dicts.append({"char": char, "count": dict[char]})
+    return list_of_dicts
 
 main()
