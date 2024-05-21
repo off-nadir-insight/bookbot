42cf85f83ed38e21371074ec77adcc65b5653229
off-nadir-insight <off.nadir.insight@gmail.com>
2024-05-21 10:31:38 -0700

    adds word count functionality

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ def main():
     book_path = "books/frankenstein.txt"
     book_text = get_book_text(book_path)
     first_499_characters = book_text[:499]
-    print(first_499_characters)
+    print(f"{get_word_count(book_text)} words in the book.")
 
 def get_book_text(path):
     with open(path) as file:
         return file.read()
+
+def get_word_count(input_string):
+    words_list = input_string.split()
+    return len(words_list)
 
 main()
