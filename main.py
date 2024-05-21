fa6bbf9ec11d6e82284cbe92fbbad3dee26df502
off-nadir-insight <off.nadir.insight@gmail.com>
2024-05-21 06:23:23 -0700

    refactors to match instructor solution

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
+    book_path = "books/frankenstein.txt"
+    book_text = get_book_text(book_path)
+    print(book_text)
+
+def get_book_text(path):
+    with open(path) as file:
+        return file.read()
 
 main()
