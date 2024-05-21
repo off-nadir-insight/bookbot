2a1940d504aa1c75443c9217fd5162205943788c
off-nadir-insight <off.nadir.insight@gmail.com>
2024-05-21 06:26:18 -0700

    updates main to only call reduced character set

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def main():
     book_path = "books/frankenstein.txt"
     book_text = get_book_text(book_path)
-    print(book_text)
+    first_499_characters = book_text[:499]
+    print(first_499_characters)
 
 def get_book_text(path):
     with open(path) as file:
